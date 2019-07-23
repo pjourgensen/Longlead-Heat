90657dda0342ac779c58e8af693d527b9c3de8c9
pjourgensen <pjourgensen@gmail.com>
2019-07-23 16:24:23 -0700

    Combine, remove trend, and remove seasonality added

--- sstpreparer.py
+++ sstpreparer.py
@@ -18,6 +18,13 @@ class SSTPreparer:
       self.yr_begin = config['yr_begin']
       self.yr_end = config['yr_end']
       self.out_path = config['out_path']
+      self.data_fn = config['data_fn']
+      self.min_lat = config['min_lat']
+      self.max_lat = config['max_lat']
+      self.min_lon = config['min_lon']
+      self.max_lon = config['max_lon']
+      self.nbases = config['nbases']
+      self.data = None
 
   def load_data(self):
       cont = input("Loading may take awhile. Are you sure you want to download data from {} to {}? ('y' or 'n')".format(self.yr_begin,self.yr_end))
@@ -44,17 +51,76 @@ class SSTPreparer:
               ftp.retrbinary("RETR "+j,open(self.out_path+j[-7:],"wb").write)
 
 
-  def combine_data(self):
-      pass
+  def combine_data(self,save_data=False):
+      filenames = [str(i)+'.nc' for i in list(range(self.yr_begin,self.yr_end+1))]
+
+      combined = xr.open_dataarray('Data/'+filenames[0])
+      lat_idx = np.where(np.logical_and(combined['lat'].values >= self.min_lat,combined['lat'].values <= self.max_lat))[0]
+      lon_idx = np.where(np.logical_and(combined['lon'].values >= self.min_lon,combined['lon'].values <= self.max_lon))[0]
+      combined = combined[:,lat_idx,lon_idx]
+
+      for i in filenames[1:]:
+          print('Adding: {}'.format(i))
+          curr = xr.open_dataarray('Data/'+i)[:,lat_idx,lon_idx]
+          combined = xr.concat([combined,curr],dim='time')
+
+      combined = combined.fillna(0)
+
+      if save_data:
+          combined.to_netcdf(self.out_path+self.data_fn)
+      else:
+          self.data = combined
+
 
   def train_test_split(self):
       pass
 
   def remove_trend(self):
+      if self.data == None:
+          self.data = xr.open_dataarray(self.out_path+self.data_fn)
+
+      data = self.data.load()
+      time_len = len(data['time'])
+      lat_len = len(data['lat'])
+      lon_len = len(data['lon'])
+
+      doy_vec = np.arange(1,time_len+1)
+
+      data = data.stack(ll=('lat','lon'))
+      coefs = np.polyfit(doy_vec,data,deg=1)
+
+      for i in range(2):
+          data -= (np.power(doy_vec,2-(i+1))).reshape((time_len,1))*coefs[i].reshape((1,lat_len*lon_len))
+
+      self.data = data.unstack()
+
+  def remove_daily_trend(self):
       pass
 
   def remove_seasonality(self):
-      pass
+      if self.data == None:
+          self.data = xr.open_dataarray(self.out_path+self.data_fn)
+
+      data = self.data.load()  # need to use a couple times
+      time_len = len(data['time'])
+
+      doy_vec = np.arange(1,time_len+1)
+
+      t_basis = (doy_vec - 0.5)/365
+      nt = len(t_basis)
+      bases = np.empty((self.nbases, nt), dtype=complex)
+      for counter in range(self.nbases):
+          bases[counter, :] = np.exp(2*(counter + 1)*np.pi*1j*t_basis)
+
+
+      data -= data.reduce(np.mean, dim='time')
+
+      coeff = 2/nt*(np.sum(bases[..., np.newaxis, np.newaxis]*data.values[np.newaxis, ...], axis=1))
+
+      rec = np.real(np.conj(coeff[:, np.newaxis, ...])*bases[..., np.newaxis, np.newaxis])
+
+      self.data = data - np.sum(rec, axis=0)
+
 
   def prepare_target(self):
       pass
